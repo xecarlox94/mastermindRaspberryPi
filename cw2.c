130aaf21d2cb6ae08b92fc5296b4075a2b4f5575
jcc8 <jcc8@hw.ac.uk>
2020-03-09 15:17:26 +0000

    first setup, first

---
+++ cw2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+struct Row
+{
+    short int colour;
+};
+
+
+
+struct Mastermind
+{
+    struct Row secret;
+
+    struct Row * guesses;
+};
+
+
+
+
+int main(int argc, char ** argv)
+{
+
+
+
+
+
+    return 0;
+}
